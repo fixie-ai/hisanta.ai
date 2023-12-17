fa8f86ffdc07c2d18d0ea9b97b630d0c1bca62ff
Patrick Li <patrick8289@gmail.com>
2023-12-17 11:43:02 -0800

    Added AgentID to CharacterID endpoints for KV store (#74)
    * first commit
    
    * addressing comments

--- lib/storage.ts
+++ lib/storage.ts
@@ -14,3 +14,17 @@ export async function loadCharacter(characterId: string): Promise<CharacterType>
 export async function saveCharacter(character: CharacterType): Promise<void> {
   await kv.json.set(`character:${character.characterId}`, '$', character);
 }
+
+/** Load the Character ID mapped to the given Agent ID from KV. */
+export async function loadCharacterIdByAgentId(agentId: string): Promise<string> {
+  const characterId = await kv.json.get(`agent:${agentId}`);
+  if (characterId === null) {
+    throw new Error(`Character ID not found for Agent ${agentId}`);
+  }
+  return characterId;
+}
+
+/** Store the mapping of Agent ID to Character ID to KV. */
+export async function saveAgentCharacterMapping(agentId: string, characterId: string): Promise<void> {
+  await kv.set(`agent:${agentId}`, characterId);
+}
