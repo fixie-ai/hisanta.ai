95bef315d91b6391a0f21d44039b0c36c7eaa50c
Patrick Li <patrick8289@gmail.com>
2023-12-21 15:12:34 -0800

    format + voice issue solved

--- app/components/Buttons/index.tsx
+++ app/components/Buttons/index.tsx
@@ -35,11 +35,30 @@ const EpicButton = ({ children, type, className, disabled, onClick, isLoading }:
     <button disabled={disabled} onClick={onClick} className={`pushable ${className}`}>
       <span className={`shadow ${shadowClass}`}></span>
       <span className={`edge ${edgeClass}`}></span>
-      <span className={`front ${frontClass}`} style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%' }}>
+      <span
+        className={`front ${frontClass}`}
+        style={{
+          position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
         {isLoading ? (
-          <svg className="animate-spin h-10 w-10 text-Holiday-Blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <svg
+            className="animate-spin h-10 w-10 text-Holiday-Blue"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
           </svg>
         ) : (
           children
