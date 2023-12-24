8cb9b59b3f5d3f0a594c2e041f0f86c7ae580f3c
Matt Welsh <matt@fixie.ai>
2023-12-24 15:14:04 -0800

    Add AI.TOWN branding (#91)

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -12,9 +12,17 @@ export default function Header() {
     <header className="flex flex-row justify-between items-center top-0 mt-4">
       <div className="self-center text-3xl pl-4 mt-2 md:pl-8">
         <Link href="/">
-          <span className="text-Holiday-Green">Hi</span>
-          <span className="text-Holiday-Red">Santa</span>
-          <span>.ai</span>
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="flex flex-row">
+              <span className="text-Holiday-Green">Hi</span>
+              <span className="text-Holiday-Red">Santa</span>
+              <span>.ai</span>
+            </div>
+            <div className="flex flex-row items-center -mt-2 md:mt-0">
+              <span className="text-slate-500 font-[Inter-Regular] text-sm md:text-sm md:ml-2">by</span>
+              <span className="text-black text-sm md:text-lg font-[Silkscreen-Regular] ml-2">ai.town</span>
+            </div>
+          </div>
         </Link>
       </div>
       <div className="ml-auto" />
